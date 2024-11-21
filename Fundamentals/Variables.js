a922dc38458d0f3cecd4f73ec9af85426d8c4d8e
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-11-21 04:57:05 -0500

    add variable examples

---
+++ Fundamentals/Variables.js
@@ -0,0 +1,7 @@
+// Declare variables using var, let, and const
+var name = "Alice"; // Function-scoped
+let age = 25;       // Block-scoped
+const isTester = true; // Block-scoped, cannot be reassigned
+
+//print variable values
+console.log(name, age, isTester);
