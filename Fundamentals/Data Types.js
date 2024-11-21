4e79d6cf81d7fd31d9382f0fce28cf3e55a24ae3
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-11-21 05:04:01 -0500

    add data types

---
+++ Fundamentals/Data Types.js
@@ -0,0 +1,27 @@
+// String
+let message = "Hello, JavaScript!";
+console.log(typeof message); // "string"
+
+// Number
+let score = 95.5;
+console.log(typeof score); // "number"
+
+// Boolean
+let isActive = true;
+console.log(typeof isActive); // "boolean"
+
+// Array
+let fruits = ["Apple", "Banana", "Cherry"];
+console.log(typeof fruits); // "object"
+
+// Object
+let user = { name: "Bob", age: 30 };
+console.log(typeof user); // "object"
+
+// Undefined
+let test;
+console.log(typeof test); // "undefined"
+
+// Null
+let emptyValue = null;
+console.log(typeof emptyValue); // "object"
