3bf8cdac81a2a49293e574d301e416fb7ecac43f
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-11-21 05:10:33 -0500

    add operators example

---
+++ Fundamentals/Operators.js
@@ -0,0 +1,25 @@
+let a = 50
+let b = 30
+
+let sum = a + b
+let sub = a - b
+let multiply = a - b
+let divide = a / b
+
+let isEqual = a == b;
+let isStrictEqual = a === a;
+
+// Logical operators
+let andResult = true && false; // false
+let orResult = true || false;  // true
+let notResult = !true;         // false
+
+console.log('Summation: ' + sum);
+console.log('Subtract: ' + sub);
+console.log('Multiply: ' + multiply);
+console.log('Divide: ' + divide);
+console.log('IsEqual: ' + isEqual);
+console.log('OrEqual: ' + orResult);
+console.log('NotEqual: ' + notResult);
+console.log('isStrictEqual: ' + isStrictEqual);
+console.log('andResult: ' + andResult);
