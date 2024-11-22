0cedda6116a28169c7935565daefb621713eb5d9
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-11-21 23:17:28 -0500

    add conditions

---
+++ fundamentals/Conditional Statements.js
@@ -0,0 +1,23 @@
+let score = 85;
+
+// if-else
+if (score >= 90) {
+    console.log("Grade: A+");
+} else if (score >= 75) {
+    console.log("Grade: A");
+} else {
+    console.log("Grade: A-");
+}
+
+// switch
+let day = 2;
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    default:
+        console.log("Invalid day");
+}
