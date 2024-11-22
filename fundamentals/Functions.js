5efc32ea60315e880f484865bb05ec6d6bdb9162
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-11-22 11:15:15 -0500

    add function examples

---
+++ fundamentals/Functions.js
@@ -0,0 +1,13 @@
+function sum(num1, num2) {
+    return num1 + num2;
+}
+
+console.log('Summation: '+sum(2, 3));
+
+// function with a parameter called 'name'
+function greet(name) {
+    console.log(`Hello ${name}`);
+}
+
+// pass argument to the function
+greet("Ebrahim");
