016abb18ac016c492833b29d79152fd9d28e4e8a
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-11-21 23:20:26 -0500

    add loops

---
+++ fundamentals/Loops.js
@@ -0,0 +1,17 @@
+// For loop
+for (let i = 1; i <= 5; i++) {
+    console.log("Iteration:", i);
+}
+
+// While loop
+let count = 1;
+while (count <= 3) {
+    console.log("Count:", count);
+    count++;
+}
+
+// ForEach (on arrays)
+let numbers = [10, 20, 30];
+numbers.forEach((num) => {
+    console.log("Number:", num);
+});
