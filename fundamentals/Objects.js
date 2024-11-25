ae9fc0e2473dec7961e47c71889866d3a1bc00ea
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-11-25 06:13:12 -0500

    add object examples

--- fundamentals/Objects.js
+++ fundamentals/Objects.js
@@ -1,12 +1,17 @@
 // Object creation
 let person = {
-    name: "Alice",
-    age: 30,
+    name: "Ebrahim",
+    age: 26,
     greet: function () {
         return `Hello, my name is ${this.name}.`;
     },
+    info: function () {
+        return `Hello, my name is ${this.name}. and my age is ${this.age}.`;
+    }
 };
 
 // Access properties
+console.log(person);
 console.log(person.name); // Alice
 console.log(person.greet()); // Hello, my name is Alice.
+console.log(person.info())
