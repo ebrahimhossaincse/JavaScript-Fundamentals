60d2edad1ec701baa7d414023d6f8cbb5ee35ebe
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-11-30 21:04:48 -0500

    add

--- fundamentals/Variables.js
+++ fundamentals/Variables.js
@@ -1,7 +1,18 @@
-// Declare variables using var, let, and const
-var name = "Alice"; // Function-scoped
-let age = 25;       // Block-scoped
-const isTester = true; // Block-scoped, cannot be reassigned
+// Declaring different variables of different data types
+let firstName = 'Ebrahim';
+let lastName = 'Hossain';
+let location = 'Dhaka';
+let country = 'Bangladesh';
+let city = 'Dhaka';
+let age = 100;
+let isMarried = true;
+const gravity = 9.81;
+const boilingPoint = 100;
+const PI = 3.14; // can can be accessed like this too: Math.PI
+console.log(firstName, lastName, location, country, age, gravity, PI);
 
-//print variable values
-console.log(name, age, isTester);
+// Variables can also be declaring in one line separated by comma
+let name = 'EBrahim',
+    job = 'Teacher',
+    live = 'Bangladesh';
+console.log(name, job, live);
